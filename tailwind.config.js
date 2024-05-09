6a495e84f574d650029b9c55532db8cd0e337173
samira abdi <samiraabdiurmia@gmail.com>
2024-05-09 21:30:19 +0330

    feat (pages) : add Jobs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,37 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-
-      "color-logo": " #fae6e6",
-      "color-background": " #fffefb",
-      "color-border": " #f0ede8",
-      "color-footer": " #806e6e",
-      "color-light": " #ffffff",
-      "color-ikea-blue": " #0058a3",
-      "color-ikea-yellow": " #ffdb00",
-      "color-ikea-yellow-hover": " #ffe53a",
-      "color-ikea-yellow-light": " #fff094",
-      "color-text-dark": " rgba(0, 0, 0, 0.74)",
-      "color-text-darker": " #111111",
-      "color-text-black": " #000",
-      "color-grey": " #dfdfdf",
-      "color-grey-dark": " #999999",
-    },
     container: {
       center: true,
     },
     extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+
+        "ikea-logo": " #fae6e6",
+        "ikea-background": " #fffefb",
+        "ikea-border": " #f0ede8",
+        "ikea-footer": " #806e6e",
+        "ikea-light": " #ffffff",
+        "ikea-blue": " #0058a3",
+        "ikea-yellow": " #ffdb00",
+        "ikea-yellow-hover": " #ffe53a",
+        "ikea-yellow-light": " #fff094",
+        "ikea-gray1": " #F5F5F5",
+        "text-dark": " rgba(0, 0, 0, 0.74)",
+        "ikea-darker": " #111111",
+        "text-black": " #000",
+        "ikea-grey": " #dfdfdf",
+        "ikea-grey-dark": " #999999",
+        "ikea-white": " #fff",
+      },
+      borderRadius: {
+        "4xl": "1.75rem",
+      },
+      lineHeight: {
+        108: "108%",
+      },
       // objectPosition: {
       //   "50-50": "center bottom",
       // },
